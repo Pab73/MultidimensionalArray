7aca7dc4f440e8b0b6f935184dcf74c9f08ea40d
PauloZM <Munoz.pablito@Gmail.com>
2021-01-08 15:06:21 +0100

    Paulo initial commit

---
+++ WindowsFormsApP14.5/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace WindowsFormsApP14._5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtGetallen = new System.Windows.Forms.TextBox();
+            this.BtnVerwisselen = new System.Windows.Forms.Button();
+            this.BtnVerplaatsen = new System.Windows.Forms.Button();
+            this.TxtVerwisssel = new System.Windows.Forms.TextBox();
+            this.TxtVerplaats = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIndex1 = new System.Windows.Forms.TextBox();
+            this.txtIndex2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // TxtGetallen
+            // 
+            this.TxtGetallen.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtGetallen.Location = new System.Drawing.Point(28, 123);
+            this.TxtGetallen.Multiline = true;
+            this.TxtGetallen.Name = "TxtGetallen";
+            this.TxtGetallen.Size = new System.Drawing.Size(248, 288);
+            this.TxtGetallen.TabIndex = 0;
+            // 
+            // BtnVerwisselen
+            // 
+            this.BtnVerwisselen.Location = new System.Drawing.Point(282, 56);
+            this.BtnVerwisselen.Name = "BtnVerwisselen";
+            this.BtnVerwisselen.Size = new System.Drawing.Size(171, 23);
+            this.BtnVerwisselen.TabIndex = 1;
+            this.BtnVerwisselen.Text = "Verwisselen";
+            this.BtnVerwisselen.UseVisualStyleBackColor = true;
+            this.BtnVerwisselen.Click += new System.EventHandler(this.BtnVerwisselen_Click);
+            // 
+            // BtnVerplaatsen
+            // 
+            this.BtnVerplaatsen.Location = new System.Drawing.Point(561, 56);
+            this.BtnVerplaatsen.Name = "BtnVerplaatsen";
+            this.BtnVerplaatsen.Size = new System.Drawing.Size(171, 23);
+            this.BtnVerplaatsen.TabIndex = 2;
+            this.BtnVerplaatsen.Text = "Verplaatsen";
+            this.BtnVerplaatsen.UseVisualStyleBackColor = true;
+            this.BtnVerplaatsen.Click += new System.EventHandler(this.BtnVerplaatsen_Click);
+            // 
+            // TxtVerwisssel
+            // 
+            this.TxtVerwisssel.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtVerwisssel.Location = new System.Drawing.Point(282, 123);
+            this.TxtVerwisssel.Multiline = true;
+            this.TxtVerwisssel.Name = "TxtVerwisssel";
+            this.TxtVerwisssel.Size = new System.Drawing.Size(238, 288);
+            this.TxtVerwisssel.TabIndex = 3;
+            // 
+            // TxtVerplaats
+            // 
+            this.TxtVerplaats.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtVerplaats.Location = new System.Drawing.Point(526, 123);
+            this.TxtVerplaats.Multiline = true;
+            this.TxtVerplaats.Name = "TxtVerplaats";
+            this.TxtVerplaats.Size = new System.Drawing.Size(262, 288);
+            this.TxtVerplaats.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Index1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Index2";
+            // 
+            // txtIndex1
+            // 
+            this.txtIndex1.Location = new System.Drawing.Point(94, 24);
+            this.txtIndex1.Name = "txtIndex1";
+            this.txtIndex1.Size = new System.Drawing.Size(100, 22);
+            this.txtIndex1.TabIndex = 7;
+            // 
+            // txtIndex2
+            // 
+            this.txtIndex2.Location = new System.Drawing.Point(94, 66);
+            this.txtIndex2.Name = "txtIndex2";
+            this.txtIndex2.Size = new System.Drawing.Size(100, 22);
+            this.txtIndex2.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtIndex2);
+            this.Controls.Add(this.txtIndex1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtVerplaats);
+            this.Controls.Add(this.TxtVerwisssel);
+            this.Controls.Add(this.BtnVerplaatsen);
+            this.Controls.Add(this.BtnVerwisselen);
+            this.Controls.Add(this.TxtGetallen);
+            this.Name = "Form1";
+            this.Text = "git";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtGetallen;
+        private System.Windows.Forms.Button BtnVerwisselen;
+        private System.Windows.Forms.Button BtnVerplaatsen;
+        private System.Windows.Forms.TextBox TxtVerwisssel;
+        private System.Windows.Forms.TextBox TxtVerplaats;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtIndex1;
+        private System.Windows.Forms.TextBox txtIndex2;
+    }
+}
+
---
+++ WindowsFormsApP14.5/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApP14._5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static int[,] arrGetallen = new int[3, 5];
+        //int[,] arrWisselGetallen = new int[3, 5];
+        //int[,] arrVerplaatsGetallen = new int[3, 5];
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random myRandom = new Random();
+
+
+            for (int rij = 0; rij <= arrGetallen.GetUpperBound(0); rij++)
+            {
+                for (int kolom = 0; kolom <= arrGetallen.GetUpperBound(1); kolom++)
+                {
+                    arrGetallen[rij, kolom] = myRandom.Next(0, 101);
+                    TxtGetallen.Text += (arrGetallen[rij, kolom].ToString().PadLeft(5));
+                }
+                TxtGetallen.Text += Environment.NewLine;
+            }
+        }
+        static void Swap(int kolomA, int kolomB)
+        {
+            int temp;
+            for (int i = 0; i < 3; i++)
+            {
+                temp = arrGetallen[i, kolomA];
+                arrGetallen[i, kolomA] = arrGetallen[i, kolomB];
+                arrGetallen[i, kolomB] = temp;
+            }
+        }
+        private void BtnVerwisselen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Swap(int.Parse(txtIndex1.Text), int.Parse(txtIndex2.Text));
+                for (int rij = 0; rij <= arrGetallen.GetUpperBound(0); rij++)
+                {
+                    for (int kolom = 0; kolom <= arrGetallen.GetUpperBound(1); kolom++)
+                    {
+                        //arrGetallen[rij, kolom] = myRandom.Next(0, 101);
+                        TxtVerwisssel.Text += (arrGetallen[rij, kolom].ToString().PadLeft(5));
+                    }
+                    TxtVerwisssel.Text += Environment.NewLine;
+                }
+                txtIndex1.Text = string.Empty;
+                txtIndex2.Text = string.Empty;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "fout", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+
+
+        }
+        private void BtnVerplaatsen_Click(object sender, EventArgs e)
+        {
+            int[,] arrTemp = new int[arrGetallen.GetUpperBound(0) + 1, arrGetallen.GetUpperBound(1) + 1];
+            Swap(int.Parse(txtIndex2.Text), arrTemp.GetUpperBound(1));
+            Swap(int.Parse(txtIndex1.Text), int.Parse(txtIndex2.Text));
+            Swap(int.Parse(txtIndex1.Text) - 1, int.Parse(txtIndex1.Text));
+            Swap(int.Parse(txtIndex1.Text) - 2, int.Parse(txtIndex1.Text) - 1);
+            //Swap(2, 4);
+            //Swap(2, 0);
+            //Swap(2, 1);
+            //Swap(0, 4);
+            for (int rij = 0; rij <= arrGetallen.GetUpperBound(0); rij++)
+            {
+                for (int kolom = 0; kolom <= arrGetallen.GetUpperBound(1); kolom++)
+                {
+                    //arrGetallen[rij, kolom] = myRandom.Next(0, 101);
+                    TxtVerplaats.Text += (arrGetallen[rij, kolom].ToString().PadLeft(5));
+                }
+                TxtVerplaats.Text += Environment.NewLine;
+            }
+        }
+    }
+}
